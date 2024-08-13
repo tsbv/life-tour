a2a20befe54cd75df13ec6afe473316fd8cb15ed
Georgij Tsabiev <gtsabiev@inbox.ru>
2024-08-14 01:44:28 +0300

    Подключает swiper библиотеку и её начальный код

--- source/js/main.js
+++ source/js/main.js
@@ -1,7 +1,3 @@
-// https://swiperjs.com/get-started#installation
-// import Swiper from "swiper";
-// import {Navigation, Pagination} from "swiper/modules";
-// import 'swiper/css';
 import { toggleMobileHandler } from './mobile-menu';
 
 toggleMobileHandler(); // Мобильное меню
