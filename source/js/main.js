a88139528c4a9f49f346a5323033dfcc511b5d94
Georgij Tsabiev <gtsabiev@inbox.ru>
2024-08-14 22:56:34 +0300

    Поправляет комментарии в main.js

--- source/js/main.js
+++ source/js/main.js
@@ -1,5 +1,5 @@
 import { toggleMobileHandler } from './mobile-menu';
 import { initTourSlider } from './tour-slider';
 
-toggleMobileHandler(); // Мобильное меню
-initTourSlider();
+toggleMobileHandler(); // Открытие мобильное меню
+initTourSlider(); // Инициализация слайдера туров.
