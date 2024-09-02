4c0d6e48b6331e350212764556b704d967a05cf8
Georgij Tsabiev <gtsabiev@inbox.ru>
2024-09-02 15:21:59 +0300

    Блокирует скролл при открытом меню

--- source/js/mobile-menu.js
+++ source/js/mobile-menu.js
@@ -1,8 +1,11 @@
 const navMenuEl = document.querySelector('.nav__menu');
 const navToggleEl = document.querySelector('.js-toggle-button');
+const bodyEl = document.querySelector('body');
+
 const toggleMobileHandler = () => {
   navToggleEl.addEventListener('click', () => {
     navMenuEl.classList.toggle('nav__menu--open');
+    bodyEl.style.overflow = bodyEl.style.overflow === 'hidden' ? 'auto' : 'hidden';
   });
 };
 
